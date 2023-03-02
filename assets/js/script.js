060e5f849afaec4ddcaa11678836ecbc7a37448f
gaurabwebdev <gaurabbose40@gmail.com>
2023-03-03 02:31:02 +0600

    card loaded and spinner set

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const loadTools = async (num) => {
+    showSpinnerBtn(true);
     const URL = `https://openapi.programming-hero.com/api/ai/tools`;
     try{
         const getTools = await fetch(URL);
@@ -17,25 +18,56 @@ const loadTools = async (num) => {
 const displayTools = (tools) => {
         console.log(tools);
         const cardContainer = document.getElementById('cards-container');
+        cardContainer.innerHTML = '';
         tools.forEach((tool) => {
+            const {name, image, features, published_in} = tool;
+            console.log(image, features,published_in);
             const singleCard = document.createElement('div');
-            singleCard.classList.add('col');
+            singleCard.classList.add('col', 'border-1');
             singleCard.innerHTML = `
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
+                <div class="card p-3">
+                    <img class="rounded thumbnail-image" src="${image}" class="card-img-top" alt="tool-image">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h4 class="card-title">Features</h4>
+                        <ol class="card-text  features-list">
+                            <li>${features[0]}</li>
+                            <li>${features[1]}</li>
+                            <li>${features[2]}</li>
+                        </ol>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between align-items-center">
+                        <div>
+                            <h5>${name}</h5>
+                            <p class="m-0">
+                            <i class="fa-solid fa-calendar-days"></i>
+                            ${published_in}</p>
+                        </div>
+                        <span onclick="showToolModal" class="rounded-circle px-2 py-1 bg-primary arrow-btn">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </span>
                     </div>
                 </div>
             `;
+            showSpinnerBtn(false);
             cardContainer.appendChild(singleCard);
         })
 }
 
 
-
-
+const showSpinnerBtn = (status) =>{
+    const spinner = document.getElementById('spinner');
+    const showAllBtn = document.getElementById('show-all-btn');
+    const sortBtn = document.getElementById('sort-btn');
+    if(status){
+        spinner.classList.remove('d-none');
+        showAllBtn.classList.add('d-none');
+        sortBtn.classList.add('d-none');
+    } else {
+        spinner.classList.add('d-none');
+        showAllBtn.classList.remove('d-none');
+        sortBtn.classList.remove('d-none');
+    }
+}
 
 
 
