a7bff64d847fed510960bb4450babd4dd47ebd09
gaurabwebdev <gaurabbose40@gmail.com>
2023-03-03 20:01:41 +0600

    all set up except features & integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,12 +26,10 @@ const displayTools = (tools) => {
             singleCard.innerHTML = `
                 <div class="card p-3">
                     <img class="rounded thumbnail-image" src="${image}" class="card-img-top" alt="tool-image">
-                    <div class="card-body">
+                    <div onload="showDefaultFeatures(${features})" class="card-body">
                         <h4 class="card-title">Features</h4>
                         <ol class="card-text  features-list">
-                            <li>${features[0]}</li>
-                            <li>${features[1]}</li>
-                            <li>${features[2]}</li>
+                            
                         </ol>
                     </div>
                     <div class="card-footer d-flex justify-content-between align-items-center">
@@ -52,6 +50,9 @@ const displayTools = (tools) => {
         })
 }
 
+const showDefaultFeatures = (features) => {
+    console.log(features);
+}
 
 const showSpinnerBtn = (status) =>{
     const spinner = document.getElementById('spinner');
@@ -83,56 +84,60 @@ const showToolModal = async (toolId) =>{
 
 
 const showModal = (data) =>{
-    // console.log(data);
+    console.log(data);
     const {image_link, input_output_examples, accuracy, description, pricing, features, integrations} = data;
-    console.log(image_link, input_output_examples, accuracy, description, pricing, features, integrations);
+    const [firsInOut, secondInOut] = input_output_examples;
+    // console.log(image_link, input_output_examples, accuracy, description, pricing, features, integrations);
     const modalBody = document.getElementById('modalBody');
     modalBody.innerHTML = `
-        <div class="container position-relative">
-            <button id="modalClose" type="button" class="btn-close position-absolute top-0 start-100 rounded-pill bg-danger p-2" data-bs-dismiss="modal" aria-label="Close"></button>
-            <div class="row row-cols-1 row-cols-md-2 g-4">
-                <div class="col border rounded">
-                    <div class="card p-2 border-0">
-                        <div class="card-body">
-                        <h5 class="card-title">${description}</h5>
-                            <div class="card-text d-flex gap-1">
-                                <div class=" d-flex flex-column justify-content-center align-items-center bg-warning-subtle rounded text-center p-1 price-container">
-                                    <span>
-                                    ${pricing[0].plan?pricing[0].plan:'Free of Cost'}
-                                    </span>
-                                    <span>
-                                    ${pricing[0].price?pricing[0].price:'Basic'}
-                                    </span>
-                                </div>
-                                <div class=" d-flex flex-column justify-content-center align-items-center bg-warning-subtle rounded text-center p-1 price-container">
-                                    <span>
-                                    ${pricing[1].plan?pricing[1].plan:'Free Of Cost'}
-                                    </span>
-                                    <span>
-                                    ${pricing[1].price?pricing[1].price:'Pro'}
-                                    </span>
-                                </div>
-                                <div class=" d-flex flex-column justify-content-center align-items-center bg-warning-subtle rounded text-center p-1 price-container">
-                                    <span>
-                                    ${pricing[2].plan?pricing[2].plan:'Free Of Cost'}
-                                    </span>
-                                    <span>
-                                    ${pricing[2].price?pricing[2].price:'Pro'}
-                                    </span>
-                                </div>
+        <span class="modal-close-btn position-absolute fs-4 top-0 end-0 z-1 bg-danger rounded-circle px-2" data-bs-toggle="modal" data-bs-target="#toolModal">
+            <i class="fa-solid fa-xmark" ></i>
+        </span>
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col p-2 border-0 rounded">
+                <div class="card border border-danger py-2 px-3 rounded bg-danger-subtle">
+                    <h4>${description}</h4>
+                    <div class="card-body">
+                        <div class="d-flex justify-content-evenly">
+                            <div class="d-flex flex-column text-center p-2 bg-body-tertiary rounded fw-bold text-success">
+                                <span>${pricing[0].price}</span>
+                                <span>${pricing[0].plan}</span>
+                            </div>
+                            <div class="d-flex flex-column text-center p-2 bg-body-tertiary rounded fw-bold text-warning">
+                                <span>${pricing[1].price}</span>
+                                <span>${pricing[1].plan}</span>
+                            </div>
+                            <div class="d-flex flex-column text-center p-2 bg-body-tertiary rounded fw-bold text-danger">
+                                <span>${pricing[2].price}</span>
+                                <span>${pricing[2].plan}</span>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-start my-2">
+                            <div>
+                                <ul class="modal-features">
+                                    <h4>Features</h4>
+                                    
+                                </ul>
+                            </div>
+                            <div>
+                                <ul class="modal-integrations">
+                                    <h4>Integrations</h4>
+                                    
+                                </ul>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="${image_link[1]?image_link[1] : image_link[0]}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title">${input_output_examples?input_output_examples[0].input : 'Can you give any example?'}</h5>
-                            <p class="card-text">
-                            ${input_output_examples?input_output_examples[0].output : 'No! Not Yet! Take a break!!!'}
-                            </p>
-                        </div>
+            </div>
+            <div class="col p-2 border-0 rounded">
+                <div class="card border py-2 px-3 rounded">
+                    <div class="position-relative">
+                        <img src="${image_link[1]?image_link[1]: image_link[0]}" class="card-img-top" alt="...">
+                        <span class="btn btn-danger position-absolute end-0 mt-2 me-2">${accuracy.score * 100}% Accuracy</span>
+                    </div>
+                    <div class="card-body d-flex flex-column justify-content-center align-items-center text-center">
+                        <h5 class="card-title">${secondInOut.input?secondInOut.input:firsInOut.input || 'Can you give any example?'}</h5>
+                        <p class="card-text">${secondInOut.output?secondInOut.output:firsInOut.output || 'No! Not Yet! Take a break!!!'}</p>
                     </div>
                 </div>
             </div>
@@ -140,3 +145,14 @@ const showModal = (data) =>{
     `;
 }
 
+
+
+
+{/* <li>${integrations[0]}</li>
+<li>${integrations[1]}</li>
+<li>${integrations[2]}</li> 
+<li>${features.feature_name}</li>*/}
+
+{/* <li>${features[0]}</li>
+<li>${features[1]}</li>
+<li>${features[2]}</li> */}
