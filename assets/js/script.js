da6b4e6793b8d73cfcd2086da2d811a1f23700be
gaurabwebdev <gaurabbose40@gmail.com>
2023-03-03 17:30:23 +0600

    modal pricing set

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,12 +16,11 @@ const loadTools = async (num) => {
 }
 
 const displayTools = (tools) => {
-        console.log(tools);
         const cardContainer = document.getElementById('cards-container');
         cardContainer.innerHTML = '';
         tools.forEach((tool) => {
-            const {name, image, features, published_in} = tool;
-            console.log(image, features,published_in);
+            // console.log(tool);
+            const {name, image, features, published_in, id} = tool;
             const singleCard = document.createElement('div');
             singleCard.classList.add('col', 'border-1');
             singleCard.innerHTML = `
@@ -42,7 +41,7 @@ const displayTools = (tools) => {
                             <i class="fa-solid fa-calendar-days"></i>
                             ${published_in}</p>
                         </div>
-                        <span onclick="showToolModal" class="rounded-circle px-2 py-1 bg-primary arrow-btn">
+                        <span onclick="showToolModal('${id}')" class="rounded-circle px-2 py-1 bg-primary arrow-btn" data-bs-toggle="modal" data-bs-target="#toolModal">
                             <i class="fa-solid fa-arrow-right"></i>
                         </span>
                     </div>
@@ -70,8 +69,74 @@ const showSpinnerBtn = (status) =>{
 }
 
 
+const showToolModal = async (toolId) =>{
+    // console.log(toolId);
+    const URL = `https://openapi.programming-hero.com/api/ai/tool/${toolId}`;
+    try{
+        const loadToolDetails = await fetch(URL);
+        const data = await loadToolDetails.json();
+        showModal(data.data);
+    } catch (error) {
+        return alert('Something went wrong! Please try again later.');
+    }
+}
 
 
-
-
+const showModal = (data) =>{
+    // console.log(data);
+    const {image_link, input_output_examples, accuracy, description, pricing, features, integrations} = data;
+    console.log(image_link, input_output_examples, accuracy, description, pricing, features, integrations);
+    const modalBody = document.getElementById('modalBody');
+    modalBody.innerHTML = `
+        <div class="container position-relative">
+            <button id="modalClose" type="button" class="btn-close position-absolute top-0 start-100 rounded-pill bg-danger p-2" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col border rounded">
+                    <div class="card p-2 border-0">
+                        <div class="card-body">
+                        <h5 class="card-title">${description}</h5>
+                            <div class="card-text d-flex gap-1">
+                                <div class=" d-flex flex-column justify-content-center align-items-center bg-warning-subtle rounded text-center p-1 price-container">
+                                    <span>
+                                    ${pricing[0].plan?pricing[0].plan:'Free of Cost'}
+                                    </span>
+                                    <span>
+                                    ${pricing[0].price?pricing[0].price:'Basic'}
+                                    </span>
+                                </div>
+                                <div class=" d-flex flex-column justify-content-center align-items-center bg-warning-subtle rounded text-center p-1 price-container">
+                                    <span>
+                                    ${pricing[1].plan?pricing[1].plan:'Free Of Cost'}
+                                    </span>
+                                    <span>
+                                    ${pricing[1].price?pricing[1].price:'Pro'}
+                                    </span>
+                                </div>
+                                <div class=" d-flex flex-column justify-content-center align-items-center bg-warning-subtle rounded text-center p-1 price-container">
+                                    <span>
+                                    ${pricing[2].plan?pricing[2].plan:'Free Of Cost'}
+                                    </span>
+                                    <span>
+                                    ${pricing[2].price?pricing[2].price:'Pro'}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="${image_link[1]?image_link[1] : image_link[0]}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title">${input_output_examples?input_output_examples[0].input : 'Can you give any example?'}</h5>
+                            <p class="card-text">
+                            ${input_output_examples?input_output_examples[0].output : 'No! Not Yet! Take a break!!!'}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
 
